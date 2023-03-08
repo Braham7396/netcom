1f540b1f9e980cc69c9ccd50abb3092320bfce48
namkeii <namanovarma@gmail.com>
2023-03-08 18:50:46 +0530

    added readme

---
+++ README.md
@@ -0,0 +1,5 @@
+todo: edit readme
+
+./initialise.sh
+python3 train.py
+python3 chat.py
--- initialise.sh
+++ initialise.sh
@@ -1,2 +1,4 @@
+python3 -m venv venv
+. venv/bin/activate
 pip install -r requirements.txt
 python3 -c 'import nltk; nltk.download("punkt")'
